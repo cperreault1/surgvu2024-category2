fde0550371ad3dfd0dcab7872a650e10d8f2b3a7
Conor Perreault <conor.perreault@intusurg.com>
2024-07-09 11:53:39 -0400

    initial commit

---
+++ process.py
@@ -0,0 +1,152 @@
+import SimpleITK
+import numpy as np
+import cv2
+from pandas import DataFrame
+from pathlib import Path
+from scipy.ndimage import center_of_mass, label
+from pathlib import Path
+from evalutils import ClassificationAlgorithm
+from evalutils.validators import (
+    UniquePathIndicesValidator,
+    DataFrameValidator,
+)
+from typing import (Tuple)
+from evalutils.exceptions import ValidationError
+import random
+from typing import Dict
+import json
+
+####
+# Toggle the variable below to debug locally. The final container would need to have execute_in_docker=True
+####
+execute_in_docker = False
+
+class VideoLoader():
+    def load(self, *, fname):
+        path = Path(fname)
+        print(path)
+        if not path.is_file():
+            raise IOError(
+                f"Could not load {fname} using {self.__class__.__qualname__}."
+            )
+            #cap = cv2.VideoCapture(str(fname))
+        #return [{"video": cap, "path": fname}]
+        return [{"path": fname}]
+
+# only path valid
+    def hash_video(self, input_video):
+        pass
+
+
+class UniqueVideoValidator(DataFrameValidator):
+    """
+    Validates that each video in the set is unique
+    """
+
+    def validate(self, *, df: DataFrame):
+        try:
+            hashes = df["video"]
+        except KeyError:
+            raise ValidationError("Column `video` not found in DataFrame.")
+
+        if len(set(hashes)) != len(hashes):
+            raise ValidationError(
+                "The videos are not unique, please submit a unique video for "
+                "each case."
+            )
+
+
+class SurgVU_classify(ClassificationAlgorithm):
+    def __init__(self):
+        super().__init__(
+            index_key='input_video',
+            file_loaders={'input_video': VideoLoader()},
+            input_path=Path("/input/") if execute_in_docker else Path("./test/"),
+            output_file=Path("/output/step_classification.json") if execute_in_docker else Path(
+                "./output/step_classification.json"),
+            validators=dict(
+                input_video=(
+                    #UniqueVideoValidator(),
+                    UniquePathIndicesValidator(),
+                )
+            ),
+        )
+        
+        ###                                                                                                     ###
+        ###  TODO: adapt the following part for creating your model and loading weights
+        ###                                                                                                     ###
+        
+        self.step_list = ["range_of_motion",
+                          "rectal_artery_vein",
+                          "retraction_collision_avoidance",
+                          "skills_application",
+                          "suspensory_ligaments",
+                          "suturing",
+                          "uterine_horn"
+                          "other"]
+
+    def dummy_step_prediction_model(self):
+        random_step_prediction = random.randint(0, len(self.step_list)-1)
+
+        return self.step_list[random_step_prediction]
+    
+    def step_predict_json_sample(self):
+        single_output_dict = {"slice_nr": 1, "step_label": None}
+        return single_output_dict
+
+    def process_case(self, *, idx, case):
+
+        # Input video would return the collection of all frames (cap object)
+        input_video_file_path = case #VideoLoader.load(case)
+        # Detect and score candidates
+        scored_candidates = self.predict(case.path) #video file > load evalutils.py
+
+        # return
+        # Write resulting candidates to result.json for this case
+        return scored_candidates
+
+    def save(self):
+        with open(str(self._output_file), "w") as f:
+            json.dump(self._case_results[0], f)
+
+
+    def predict(self, fname) -> Dict:
+        """
+        Inputs:
+        fname -> video file path
+        
+        Output:
+        tools -> list of prediction dictionaries (per frame) in the correct format as described in documentation 
+        """
+        
+        print('Video file to be loaded: ' + str(fname))
+        cap = cv2.VideoCapture(str(fname))
+        num_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+
+        
+        ###                                                                     ###
+        ###  TODO: adapt the following part for YOUR submission: make prediction
+        ###                                                                     ###
+        
+        print(num_frames)
+
+        # generate output json
+        all_frames_predicted_outputs = []
+        for i in range(num_frames):
+            frame_dict = self.step_predict_json_sample()
+            step_detection = self.dummy_step_prediction_model()
+
+            frame_dict['slice_nr'] = i
+            
+            frame_dict["step_label"] = step_detection
+
+            all_frames_predicted_outputs.append(frame_dict)
+
+        tools = all_frames_predicted_outputs
+
+        return tools
+
+
+
+if __name__ == "__main__":
+    SurgVU_classify().process()
